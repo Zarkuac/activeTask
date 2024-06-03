09e35a70e9f4aed4330a084feb35f887d113b145
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2024-06-03 17:59:06 +0400

    test commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('/partials/products.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('products').innerHTML = data;
+        });
+
+    fetch('/partials/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
+});
+
+
+document.getElementById('add-product-btn').addEventListener('click', function() {
+    window.location.href = 'add_products.html';
+});
+
+
+
+document.getElementById('cancel-btn').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
